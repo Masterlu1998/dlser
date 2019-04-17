eedbe8f516d8d25300e9d4a5d42e1d5043c95f64
lb <lubing3302@gmail.com>
2019-04-17 13:36:59 +0800

    feat: 用init的方法改写数据库连接，去掉锁机制

--- main.go
+++ main.go
@@ -5,7 +5,6 @@ import (
 	"net/http"
 	"github.com/julienschmidt/httprouter"
 	"dlser/handler"
-	_ "github.com/go-sql-driver/mysql"
 )
 
 func main() {
--- mysql/connection.go
+++ mysql/connection.go
@@ -2,29 +2,23 @@ package mysql
 
 import (
 	"fmt"
-	"sync"
 
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/jinzhu/gorm"
 )
 
 var db *gorm.DB
-var once sync.Once
 var err error
 
-func GetDbConnection() (*gorm.DB) {
-	if db == nil {
-		once.Do(func() {
-			fmt.Println("创建一个新的连接")
-			if db == nil {
-				db, err = gorm.Open("mysql", "root:62795828lovE@tcp(116.62.156.102:3306)/dl_app?charset=utf8")
-				if err != nil {
-					fmt.Println(err)
-					panic(err)
-				}
-				// gorm全局设置
-				db.SingularTable(true)
-			}
-		})
+func init() {
+	fmt.Println("新建mysql连接")
+	db, err = gorm.Open("mysql", "root:62795828lovE@tcp(116.62.156.102:3306)/dl_app?charset=utf8")
+	if err != nil {
+		panic(err)
 	}
+	db.SingularTable(true)
+}
+
+func GetDbConnection() *gorm.DB {
 	return db
 }
